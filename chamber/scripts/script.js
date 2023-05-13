129099572923f463f884581b081c948b7b96f688
jcampos-94 <jcampos.94@hotmail.com>
2023-05-12 19:54:15 -0500

    chamber of commerce small

---
+++ chamber/scripts/script.js
@@ -0,0 +1,16 @@
+document.querySelector("#currentYear").innerHTML = new Date().getFullYear();
+
+document.querySelector("#lastUpdated").innerHTML = document.lastModified;
+
+const datefield = document.querySelector("time");
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-UK", {dateStyle: "full"}).format(now);
+datefield.textContent = fulldate;
+
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById("hamburgerBtn").classList.toggle("open");
+}
+
+const x = document.getElementById("hamburgerBtn");
+x.onclick = toggleMenu;
