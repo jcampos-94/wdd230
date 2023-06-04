d3eae23a2cb36088f975e784c8c0a7e8f5aa0cc8
jcampos-94 <jcampos.94@hotmail.com>
2023-06-04 18:19:29 -0500

    "since last visit" and lazy loading added

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -1,13 +1,19 @@
+/*---------- LAST MODIFIED ----------*/
+
 document.querySelector("#currentYear").innerHTML = new Date().getFullYear();
 document.querySelector("#lastUpdated").innerHTML = document.lastModified;
 
 document.querySelector("#currentYear2").innerHTML = new Date().getFullYear();
 document.querySelector("#lastUpdated2").innerHTML = document.lastModified;
 
+/*---------- FULL DATE ----------*/
+
 const datefield = document.querySelector("time");
 const now = new Date();
 const fulldate = new Intl.DateTimeFormat("en-UK", {dateStyle: "full"}).format(now);
 datefield.textContent = fulldate;
+
+/*---------- HAMBURGER BUTTON ----------*/
 
 function toggleMenu() {
     document.getElementById("primaryNav").classList.toggle("open");
@@ -16,6 +22,8 @@ function toggleMenu() {
 
 const x = document.getElementById("hamburgerBtn");
 x.onclick = toggleMenu;
+
+/*---------- BANNER ----------*/
 
 function closeBanner() {
     document.getElementById("banner").classList.remove("open");
@@ -31,3 +39,71 @@ if (today === 1 || today === 2) {
     banner.classList.remove("close");
     banner.classList.add("open");
 }
+
+/*---------- LAZY LOADING ----------*/
+
+/* Selecting all images*/
+const images = document.querySelectorAll("[data-srcset]");
+
+/* Modify rootMargin bottom and threshold to test for the image loading */
+const imgOptions = {
+    threshold: 1,
+    rootMargin: "0px 0px 150px 0px"
+};
+
+/* Replace src with data-src and then remove it */
+function preloadImage(img) {
+    const srcset = img.getAttribute("data-srcset");
+    if (!srcset) {
+        return;
+    }
+
+    img.srcset = srcset;
+    img.onload = () => {
+        img.removeAttribute("data-srcset");
+    };
+}
+
+/* Image Observer */
+const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            preloadImage(entry.target);
+            imgObserver.unobserve(entry.target);
+        }
+    })
+}, imgOptions);
+
+/* Iterate through each image to load them */
+images.forEach(image => {
+    imgObserver.observe(image);
+});
+
+/*---------- LAST VISIT ----------*/
+
+// Somewhere else in your code base set the last visit in localStorage
+
+function displayDaysSinceLastVisit() {
+    const visitsDisplay = document.querySelector('.daysSinceLastVisit');
+    
+    const lastVisit = Number(localStorage.getItem('lastVisit'));
+    
+    if (!lastVisit) {
+        visitsDisplay.innerHTML = 'This is your first visit.';
+        
+        return;
+    }
+    
+    const currentDate = Date.now();
+    
+    const difference = currentDate - lastVisit;
+    const differenceInDays =difference / 84600000;
+    
+    visitsDisplay.innerHTML = differenceInDays.toFixed(0);
+}
+
+displayDaysSinceLastVisit();
+
+localStorage.setItem('lastVisit', Date.now());
