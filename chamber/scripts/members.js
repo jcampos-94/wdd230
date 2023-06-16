953ff0ea03a58dddfbc8b077be0d754e8d490153
jcampos-94 <jcampos.94@hotmail.com>
2023-06-15 23:46:59 -0500

    json img test complete

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -4,7 +4,29 @@ async function getMemberData() {
   const response = await fetch(url);
   const data = await response.json();
   console.table(data.members);
-  //displayProphets(data.members);
+  displayMembers(data.members);
 }
+
+const displayMembers = (members) => {
+    const cards = document.querySelector('div.cards'); // select the output container element
+  
+    members.forEach((member) => {
+      // Create elements to add to the div.cards element
+      let card = document.createElement('section');
+
+      let portrait = document.createElement('img');
+  
+  
+      // Build the image portrait by setting all the relevant attribute
+      portrait.setAttribute('src', member.img);
+      portrait.setAttribute('alt', `${member.name}'s logo`);
+      portrait.setAttribute('loading', 'lazy');
+  
+      // Append the section(card) with the created elements
+      card.appendChild(portrait);
+  
+      cards.appendChild(card);
+    }); // end of forEach loop
+  } // end of function expression
 
 getMemberData();
