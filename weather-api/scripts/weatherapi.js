ecaf7a969711b890076e9d584d071734dd172be4
jcampos-94 <jcampos.94@hotmail.com>
2023-06-22 18:18:16 -0500

    weather-api added

---
+++ weather-api/scripts/weatherapi.js
@@ -0,0 +1,37 @@
+// select HTML elements in the document
+const currentTemp = document.querySelector("#current-temp");
+const weatherIcon = document.querySelector("#weather-icon");
+const captionDesc = document.querySelector("figcaption");
+
+//Weather URL
+const code = "cb662272e8fd9de9cc13a7ddd6b91cb1";
+const url = `https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=imperial&appid=${code}`;
+
+//Fetch
+async function apiFetch() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data); // this is for testing the call
+      displayResults(data);
+    } else {
+      throw Error(await response.text());
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function displayResults(weatherData) {
+  currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+
+  const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+  const desc = weatherData.weather[0].description;
+
+  weatherIcon.setAttribute("src", iconsrc);
+  weatherIcon.setAttribute("alt", desc);
+  captionDesc.textContent = desc;
+}
+
+apiFetch();
