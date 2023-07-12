cca219304f98bdbce6c7949b25e82f747d8ddaec
jcampos-94 <jcampos.94@hotmail.com>
2023-07-12 18:32:45 -0500

    CSS started

---
+++ bountiful/scripts/nav.js
@@ -0,0 +1,9 @@
+/*---------- HAMBURGER BUTTON ----------*/
+
+function toggleMenu() {
+    document.getElementById("navMenu").classList.toggle("open");
+    document.getElementById("hamburgerBtn").classList.toggle("open");
+}
+
+const x = document.getElementById("hamburgerBtn");
+x.onclick = toggleMenu;
