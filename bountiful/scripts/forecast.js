87019af56d5523a6feb3bef2c20dc56c8ac8dfd1
jcampos-94 <jcampos.94@hotmail.com>
2023-07-14 18:52:20 -0500

    humidity live

--- bountiful/scripts/forecast.js
+++ bountiful/scripts/forecast.js
@@ -1,3 +1,11 @@
+// select HTML elements in the document
+const humidityHmtl = document.querySelector('#humidity');
+const weatherIcon = document.querySelector("#weather-icon");
+const tempDesc = document.querySelector("#temp-desc");
+const currentSpeed = document.querySelector("#speed");
+let temperature; // for windchill calculation
+let windSpeed; // for windchill calculation
+
 //Weather URL
 const code = "cb662272e8fd9de9cc13a7ddd6b91cb1";
 const url = `https://api.openweathermap.org/data/2.5/forecast?q=Carlsbad&units=imperial&cnt=4&appid=${code}`;
@@ -18,14 +26,16 @@ async function apiFetch() {
 }
 
 function displayResults(data) {
+    humidity = data.list[0].main.humidity.toFixed(0);
+    console.log(`${humidity}%`);
+    humidityHmtl.innerHTML = `Humidity: ${humidity}%`;
+
     (data.list).forEach(day => {
         temperature = day.main.temp.toFixed(0);
         weather = day.weather[0].description;
-        humidity = day.main.humidity.toFixed(0);
 
         console.log(`${temperature}ºF`);
         console.log(weather);
-        console.log(`${humidity}%`);
     });
 }
   
