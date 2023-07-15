9d39b99943d14a82ca614966b0f546e8fde1b351
jcampos-94 <jcampos.94@hotmail.com>
2023-07-15 17:07:28 -0500

    forecast complete

--- bountiful/scripts/forecast.js
+++ bountiful/scripts/forecast.js
@@ -1,10 +1,5 @@
 // select HTML elements in the document
-const humidityHmtl = document.querySelector('#humidity');
-const weatherIcon = document.querySelector("#weather-icon");
-const tempDesc = document.querySelector("#temp-desc");
-const currentSpeed = document.querySelector("#speed");
-let temperature; // for windchill calculation
-let windSpeed; // for windchill calculation
+const humidityHmtl = document.querySelector("#humidity");
 
 //Weather URL
 const code = "cb662272e8fd9de9cc13a7ddd6b91cb1";
@@ -12,31 +7,47 @@ const url = `https://api.openweathermap.org/data/2.5/forecast?q=Carlsbad&units=i
 
 //Fetch
 async function apiFetch() {
-    try {
-      const response = await fetch(url);
-      if (response.ok) {
-        const data = await response.json();
-        displayResults(data);
-      } else {
-        throw Error(await response.text());
-      }
-    } catch (error) {
-      console.log(error);
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      displayResults(data);
+    } else {
+      throw Error(await response.text());
     }
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 function displayResults(data) {
-    humidity = data.list[0].main.humidity.toFixed(0);
-    console.log(`${humidity}%`);
-    humidityHmtl.innerHTML = `Humidity: ${humidity}%`;
+  //Display humidity
+  const humidity = data.list[0].main.humidity.toFixed(0);
+  console.log(`${humidity}%`);
+  humidityHmtl.innerHTML = `Humidity: ${humidity}%`;
 
-    (data.list).forEach(day => {
-        temperature = day.main.temp.toFixed(0);
-        weather = day.weather[0].description;
+  data.list.forEach((day, index) => {
+    //Establish required data
+    const temperature = day.main.temp.toFixed(0);
+    let weather = day.weather[0].description;
+    weather = weather.split(" ").map(word => word[0].toUpperCase() + word.slice(1)).join(" ");
+    const icon = day.weather[0].icon;
 
-        console.log(`${temperature}ºF`);
-        console.log(weather);
-    });
+    // Update content inside the div
+    const dayId = `day${index + 1}`;
+    const divElement = document.getElementById(dayId);
+
+    divElement.querySelector('.temperature').innerHTML = `${temperature}ºF`;
+
+    const weatherIcon = divElement.querySelector('.weatherIcon');
+    weatherIcon.src = `https://openweathermap.org/img/wn/${icon}@4x.png`;
+    weatherIcon.alt = `${weather}`;
+
+    divElement.querySelector('.description').innerHTML = `${weather}`;
+
+    console.log(`${temperature}ºF`);
+    console.log(weather);
+  });
 }
-  
+
 apiFetch();
