602484ae481ef4588dcaa8fd55f937ed6bde1a6e
jcampos-94 <jcampos.94@hotmail.com>
2023-07-18 18:50:26 -0500

    Fruits logic started

--- bountiful/scripts/fruits.js
+++ bountiful/scripts/fruits.js
@@ -1,1 +1,28 @@
-//Fetch
+// Fetch
+const url = "https://brotherblazzard.github.io/canvas-content/fruit.json";
+
+async function getFruitsData() {
+  const response = await fetch(url);
+  const fruitData = await response.json();
+  console.log(fruitData);
+  populateFruitOptions(fruitData);
+}
+
+// Populate the form
+function populateFruitOptions(fruitData) {
+  const fruitSelects = document.querySelectorAll("select[id^='fruit']");
+
+  fruitSelects.forEach((select) => {
+    fruitData.forEach((fruit) => {
+      const option = document.createElement("option");
+      option.value = fruit.name;
+      option.text = fruit.name;
+      select.appendChild(option);
+    });
+  });
+}
+
+// 
+
+// Run
+getFruitsData();
